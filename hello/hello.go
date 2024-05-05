cca0d57e56344751dc9135153e78242907cf0e04
sohailshaukat <sohail47k@gmail.com>
2024-05-05 15:31:57 -0400

    Module-1 Completed

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samanth", "Darrin"}
+	// fmt.Println(quote.Go())
+	// message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
 }
+
+// to track dependencies -> go mod init example.com/<project-name>
+// to run -> go run .
+// to install dependencies -> go mod tidy
